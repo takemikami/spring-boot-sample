0cdba10acad88eab4ce8e450e267d884a3864808
Mikami, Takeshi <takeshi.mikami@gmail.com>
2018-02-21 00:05:37 +0900

    add hello world kotlin

---
+++ hello-world-kotlin/README.md
@@ -0,0 +1,7 @@
+# hello world kotlin
+
+Developing Spring Boot applications with Kotlin | spring.io
+https://spring.io/blog/2016/02/15/developing-spring-boot-applications-with-kotlin
+
+SPRING INITIALIZR
+https://start.spring.io/
---
+++ hello-world-kotlin/src/main/kotlin/hello/Application.kt
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
